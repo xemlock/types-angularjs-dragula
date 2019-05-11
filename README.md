74d6e328dcaf5415a16c4f2c79c1086f7d5f4954
xemlock <xemlock@gmail.com>
2019-05-11 22:47:12 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# types-angularjs-dragula
+
+## Summary
+
+This package contains TypeScript type definitions for [angularjs-dragula](https://github.com/bevacqua/angularjs-dragula).
+
+## Installation
+
+```
+npm install --save https://github.com/xemlock/types-angularjs-dragula/tarball/v1.0.0
+```
---
+++ angular-dragula.d.ts
@@ -0,0 +1,21 @@
+import * as ng from 'angular';
+import * as d from 'dragula';
+
+declare module 'angular' {
+  namespace dragula {
+    interface DragulaService {
+      add(scope: ng.IScope, name: string, drake: d.Drake): Bag;
+      find(scope: ng.IScope, name: string): Bag | undefined;
+      destroy(scope: ng.IScope, name: string): void;
+      handleModels(scope: ng.IScope, drake: d.Drake): void;
+      options(scope: ng.IScope, name: string, options: d.DragulaOptions): void;
+    }
+    interface Bag {
+      name: string;
+      drake: d.Drake;
+    }
+  }
+}
+
+declare var _: (angular: ng.IAngularStatic) => string;
+export = _;
